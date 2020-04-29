7c1eadfa7da0dcab9e0abb260a5a4db8c81ca08b
BodiaFityo <bohdan.fityo@gmail.com>
2020-04-29 17:21:23 +0300

    add pages: dashboard, create, login. add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,33 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {CreateTaskComponent} from './create-task/create-task.component';
+import {LoginComponent} from './login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '', component: MainLayoutComponent, children: [
+            {
+                path: '', redirectTo: '/', pathMatch: 'full'
+            },
+            {
+                path: '', component: DashboardComponent
+            },
+            {
+                path: 'create', component: CreateTaskComponent
+            },
+            {
+                path: 'login', component: LoginComponent
+            }
+        ]
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavComponent } from './shared/components/nav/nav.component';
+import { CreateTaskComponent } from './create-task/create-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainLayoutComponent,
+    DashboardComponent,
+    NavComponent,
+    CreateTaskComponent,
   ],
   imports: [
     BrowserModule,
